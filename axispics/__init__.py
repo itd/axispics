e2fd6b112b38e429c4686ceb0b7852a160344921
Kurt Bendl <kurt.bendl@nrel.gov>
2018-08-31 13:23:15 -0600

    set def route to capture images

--- axispics/__init__.py
+++ axispics/__init__.py
@@ -5,7 +5,7 @@ def main(global_config, **settings):
     """
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
-    config.add_route('pics', '/pics')
+    config.add_route('pics', '/')
+    config.add_route('home', '/home')
     config.scan()
     return config.make_wsgi_app()
